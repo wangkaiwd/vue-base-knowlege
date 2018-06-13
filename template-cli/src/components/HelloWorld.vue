2fa964be2fa9e9aa4569d28b67b85f06a5c489f2
wangkaiwd <1162486176@qq.com>
2018-06-13 14:42:08 +0800

    vue-cli的初步使用

---
+++ template-cli/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    hello world
+    <p :title="msg">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: `页面加载于${new Date().toLocaleString()}`
+    };
+  }
+};
+</script>
+
+<style>
+</style>
