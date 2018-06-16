4f42db5cc84273f5df4450ab84cce7cb2cb6cd6c
wangkaiwd <1162486176@qq.com>
2018-06-16 16:18:07 +0800

    动态组件实现tab切换

---
+++ template-cli/src/components/sub/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="blog-post">
+    <h2>{{postTitle.title}}</h2>
+    <p>{{postTitle.id}}</p>
+    <!-- 通过组件传值的方式实现让父组件的postFontSize发生变化,react实现方式 -->
+    <button @click="() => toBig(0.5)">large</button>
+    <!-- 通过自定义事件实现相同的功能 -->
+    <button @click="$emit('textTobig', 1)">_self large</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["postTitle", "toBig"],
+  created() {
+    console.log("props", this.postTitle);
+  }
+};
+</script>
+
+<style>
+</style>
