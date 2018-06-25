9b5764f290d92c7e71b6d45dd7e8c22e7fb46490
wangkaiwd <1162486176@qq.com>
2018-06-25 16:36:21 +0800

    vue路由动画

---
+++ animate/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Transitions</h1>
+    <ul>
+      <li><router-link to="/">/</router-link></li>
+      <li><router-link to="/parent">/parent</router-link></li>
+      <li><router-link to="/parent/foo">/parent/foo</router-link></li>
+      <li><router-link to="/parent/bar">/parent/bar</router-link></li>
+    </ul>
+    <transition mode="out-in">
+      <router-view class="view"></router-view>
+    </transition>
+  </div>
+</template>
+
+<style lang="less" scoped>
+</style>
